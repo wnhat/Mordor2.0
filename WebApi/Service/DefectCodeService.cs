15aacd33c4a52e39645b4a03c8e074000932f849
songjianghang <songjianghang@boe.com.cn>
2022-04-02 13:45:47 +0800

    0402for merge

---
+++ WebApi/Service/DefectCodeService.cs
@@ -0,0 +1,92 @@
+﻿using DICS_WebApi.Models;
+using WebApi.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Service
+{
+    public interface IDefectCodeService
+    {
+        Task<List<DefectCode>> GetAllDefectCodes();
+        Task<DefectCode> CreateDefectCode(DefectCode param);
+        Task UpdateDefectCode(DefectCode param);
+        Task DeleteDefectCode(string code);
+        Task DeleteDefectCodeMany(string[] code_list);
+    }
+
+    public class DefectCodeService : IDefectCodeService
+    {
+        private readonly IMongoCollection<DefectCode> _defectcodes;
+
+        public DefectCodeService(IUserDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _defectcodes = database.GetCollection<DefectCode>(settings.UsersCollectionName.DefectCode);
+        }
+
+        public Task<DefectCode> CreateDefectCode(DefectCode param)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<DefectCode>.Filter.Eq("Code", param.Code);
+                if (_defectcodes.Find(filter).FirstOrDefault() != null)
+                {
+                    throw new ApplicationException("Defect \"" + param.Code + "\" is already existed");
+                }
+
+                _defectcodes.InsertOne(param);
+
+                return param;
+            });
+        }
+
+        public Task DeleteDefectCode(string code)
+        {
+            return Task.Run(() => { return _defectcodes.DeleteOne(defectCode => defectCode.Code == code); });
+        }
+
+        public Task DeleteDefectCodeMany(string[] code_list)
+        {
+            return Task.Run(() =>
+            {
+                // 全选删除
+                if (code_list.Length == 0)
+                {
+                    return _defectcodes.DeleteMany(Builders<DefectCode>.Filter.Empty);
+                }
+                // 多选删除
+                else
+                {
+                    return _defectcodes.DeleteMany(defectCode => code_list.Contains(defectCode.Code));
+                }
+            });
+        }
+
+        public Task<List<DefectCode>> GetAllDefectCodes()
+        {
+            return Task.Run(() => { return _defectcodes.Find(defectCode => true).ToList(); });
+        }
+
+        public Task UpdateDefectCode(DefectCode param)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<DefectCode>.Filter.Eq("Id", param.Id);
+                var defectCode = _defectcodes.Find(filter).FirstOrDefault();
+
+                if (defectCode == null)
+                {
+                    throw new ApplicationException("Defect Code not found!");
+                }
+
+                _defectcodes.ReplaceOne(find => find.Id == param.Id, param);
+                
+            });
+        }
+    }
+}
---
+++ WebApi/Service/ProductInfoService.cs
@@ -0,0 +1,162 @@
+﻿using DICS_WebApi.Models;
+using WebApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Service
+{
+    public interface IProductInfoService
+    {
+        Task<List<ProductInfo>> GetProductInfos();
+        Task UploadIMG(ProductInfo product);
+        Task AddNewProduct(ProductInfo product);
+        Task UpdateProduct(ProductInfo product);
+        Task DeleteProduct(ProductInfo product);
+    }
+
+    public class ProductInfoService : IProductInfoService
+    {
+        private readonly IMongoCollection<ProductInfo> _productInfo;
+
+        public ProductInfoService(IUserDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _productInfo = database.GetCollection<ProductInfo>(settings.UsersCollectionName.ProductInfo);
+        }
+
+        public Task<List<ProductInfo>> GetProductInfos()
+        {
+            return Task.Run(() => {
+                var filter = Builders<ProductInfo>.Filter.Empty;
+                return _productInfo.Find(filter).ToList(); 
+            });
+        }
+
+        public Task UploadIMG(ProductInfo product)
+        {
+            return Task.Run(() =>
+            {
+                product.Img.ImgName = "boe.jpeg";
+                string filepath = @"E:\img\" + product.Img.ImgName;
+                product.Img.Data = ImgToByte(filepath);
+
+                var filter = Builders<ProductInfo>.Filter.Eq("Name", product.Name);
+                var update = Builders<ProductInfo>.Update.Set("Img", product.Img);
+                _productInfo.UpdateOne(filter, update);
+            });
+        }
+
+        public Task AddNewProduct(ProductInfo product)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<ProductInfo>.Filter.Eq("Name", product.Name);
+                if (_productInfo.Find(filter).FirstOrDefault() != null)
+                {
+                    throw new ApplicationException("产品已存在");
+                }
+
+                _productInfo.InsertOne(product);
+            });
+        }
+
+        public Task UpdateProduct(ProductInfo product)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<ProductInfo>.Filter.Eq("Name", product.Name);
+                ProductInfo storeData = _productInfo.Find(filter).FirstOrDefault();
+
+                if (storeData == null)
+                {
+                    throw new ApplicationException("产品信息不存在");
+                }
+
+                product.Id = storeData.Id;
+                product.Img.ImgName = storeData.Img.ImgName;
+                product.Img.Data = storeData.Img.Data;
+
+                if (CompareProperties(product, storeData))
+                {
+                    throw new ApplicationException("无信息更新，请重新检查输入");
+                }
+
+                _productInfo.ReplaceOne(filter, product);
+            });
+        }
+
+        public Task DeleteProduct(ProductInfo product)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<ProductInfo>.Filter.Eq("Name", product.Name);
+                if (_productInfo.Find(filter).FirstOrDefault() == null)
+                {
+                    throw new ApplicationException("产品信息不存在");
+                }
+
+                _productInfo.DeleteOne(filter);
+            });
+        }
+
+        private static byte[] ImgToByte(string path)
+        {
+            FileStream fs = new(path, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new(fs);
+            byte[] Data = binaryReader.ReadBytes((int)fs.Length);
+
+            return Data;
+        }
+
+        private static bool CompareProperties(object obj1, object obj2)
+        {
+            
+            var bsonElements1 = BsonDocument.Create(obj1.ToBsonDocument());
+            var bsonElements2 = BsonDocument.Create(obj2.ToBsonDocument());
+            return bsonElements1 == bsonElements2;
+
+
+//            BinaryFormatter binaryFormatter = new();
+//            MemoryStream memoryStream1;
+//            MemoryStream memoryStream2;
+//            using (memoryStream1 = new())
+//            {
+//#pragma warning disable SYSLIB0011 // obsolete method (Object )
+//                binaryFormatter.Serialize(memoryStream1, obj1);
+//            }
+//            using (memoryStream2 = new())
+//            {
+//                binaryFormatter.Serialize(memoryStream2, obj2);
+//#pragma warning restore SYSLIB0011
+//            }
+//            string MD5_1 = MD5Encrypt(memoryStream1.ToArray());
+//            string MD5_2 = MD5Encrypt(memoryStream2.ToArray());
+
+//            return (MD5_1 == MD5_2);
+        }
+
+        private static string MD5Encrypt(byte[] bytes)
+        {
+            if (bytes == null)
+            {
+                return null;
+            }
+
+            MD5 md5Hasher = MD5.Create();
+            byte[] hashBytes = md5Hasher.ComputeHash(bytes);
+            string result = BitConverter.ToString(hashBytes);
+
+            return result.Replace("-", "").ToUpper();
+        }
+
+    }
+}
